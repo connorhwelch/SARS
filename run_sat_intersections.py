cf84e304f2788f4957dd5ed3237a84442f0a4393
connorhwelch <connor.h.welch@nasa.gov>
2026-02-06 14:19:21 -0600

    --- run 1 hr 300km ---

--- run_sat_intersections.py
+++ run_sat_intersections.py
@@ -143,5 +143,5 @@ def main(args, timebuffer_hours=2, sep_dist_km=100.0):
 if __name__ == '__main__':
     args = args_for_batching()
     main(args,
-         timebuffer_hours=2,
+         timebuffer_hours=1,
          sep_dist_km=300.0)
